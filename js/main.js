f616a8d729db8fe3478b1c0bdc67c40eef396ed8
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-15 19:19:23 -0500

    Add modal fadeIn and fadeOut

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+
+   $('.signin').on('click', function(){
+     $('.modal').fadeIn('slow')
+ })
+   $('.close').on('click', function(){
+      $('.modal').fadeOut('slow')
+   })
+
+
+})
