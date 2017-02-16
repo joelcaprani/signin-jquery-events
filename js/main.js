5ca0d3654dc3de2f1d74cf16eb59facf4e3a821b
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-15 20:41:50 -0500

    Add error class and remove error class

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 $(function() {
 
-   $('.signin').on('click', function(){
-     $('.modal').fadeIn('slow')
+$('.signin').on('click', function(){
+ $('.modal').fadeIn('slow')
  })
-   $('.close').on('click', function(){
-      $('.modal').fadeOut('slow')
-   })
 
+$('.close').on('click', function(){
+ $('.modal').fadeOut('slow')
+ })
 
-})
+$(function() {
+  $('.submit').on('click', function() {
+  $("[name = 'user']").addClass('error')
+  $("[name = 'pass']").addClass('error')
+  })
+  })
+
+  $(function() {
+    $("[name = 'user']").on('click', function() {
+    $("[name = 'user']").removeClass('error')
+    })
+    })
+
+  $(function() {
+    $("[name = 'pass']").on('click', function() {
+    $("[name = 'pass']").removeClass('error')
+    })
+    })
+
+ })
+   // $('.submit').click(function(){
+   //    alert("Fill in Username and Password").click();
+  // })
